8e030236eae6ca72584762b37df727ed2f03bcd9
MintKangaroo <yjun273@gmail.com>
2021-09-30 19:39:36 +0900

    google_maps_flutter 구현

--- FooRip_APP/foorip_app_project/lib/Pages/MainPage.dart
+++ FooRip_APP/foorip_app_project/lib/Pages/MainPage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'dart:async';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -9,6 +11,14 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  //구글맵 controller 초기화
+  Completer<GoogleMapController> _controller = Completer();
+  //구글맵 카메라 위치 초기화
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(36.982110, 127.528039),
+    zoom: 14.4746,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,15 +28,26 @@ class _MainPageState extends State<MainPage> {
             child: Column(
               children: [
                 Row(
-                  children: [],
-                ), //TODO: 앱 이름과 식당 이름 추가
-                Container(),//TODO: 필터 검색 
+                  children: [
+                    Text("푸립"),
+                    Container(
+                        //TODO: SendPage로 이어짐
+                        )
+                  ],
+                ),
+                Container(), //TODO: 필터 검색
               ],
             ),
             flex: 1,
           ),
           Flexible(
-            child: Container(color: Colors.yellow), //TODO:  여기에 지도 구현하기
+            child: GoogleMap(
+              mapType: MapType.normal,
+              initialCameraPosition: _kGooglePlex,
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+              },
+            ), //구글맵 구현
             flex: 5,
           ),
         ],
