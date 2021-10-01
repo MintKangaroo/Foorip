84e09cba11f0a7ab5dc8acf885001b83252fb52b
MintKangaroo <yjun273@gmail.com>
2021-10-01 22:30:25 +0900

    Login 페이지 디자인 변경

--- FooRip_APP/foorip_app_project/lib/Pages/LoginPage.dart
+++ FooRip_APP/foorip_app_project/lib/Pages/LoginPage.dart
@@ -9,6 +9,15 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  //TextField에서 아이디, PW 입력 받아 저장하는 변수
+  final LoginIDtextController = new TextEditingController();
+  final TextEditingController LoginPWtextController =
+      new TextEditingController();
+
+  //checkBox 변수 정의
+  bool _isChecked = false;
+
+  //회원가입 버튼 or 로그인 버튼이 클릭됐을 때 Main or Register로 이동하는 함수
   void MovetoMain() {
     Get.offAndToNamed('/MainPage');
   }
@@ -19,123 +28,233 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Column(
-          children: [
-            Flexible(
-              flex: 3,
-              child: Container(
-                child: SizedBox(),
-              ),
-            ),
-            Flexible(
-              flex: 2,
-              child: Container(
-                child: Column(
-                  children: [
-                    Text("Login"),
-                    Container(
-                      width: 40,
-                      height: 2,
-                      color: Colors.yellow,
-                    ),
-                  ],
+    //Display width, height 구하기
+    var displayWidth = MediaQuery.of(context).size.width;
+    var displayHeight = MediaQuery.of(context).size.height;
+
+    return Expanded(
+      child: SafeArea(
+        child: Scaffold(
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  height: displayHeight * 0.1,
+                  child: SizedBox(),
                 ),
-              ),
-            ),
-            Flexible(
-              flex: 6,
-              child: Container(
-                child: Column(
-                  children: [
-                    Container(
-                      width: 40,
-                      height: 10,
-                      color: Colors.yellow,
-                    ),
-                    Container(
-                      width: 40,
-                      height: 10,
-                      color: Colors.yellow,
-                    ),
-                    Row(
-                      children: [
-                        //TODO: CheckBox 구현
-                        Text("로그인을 유지하겠습니까?")
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Flexible(
-              flex: 2,
-              child: Container(child: SizedBox()),
-            ),
-            Flexible(
-              flex: 4,
-              child: Container(
-                child: Column(
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        MovetoMain();
-                      },
-                      child: Container(
-                        width: 40,
-                        height: 15,
-                        color: Colors.yellow,
+                Container(
+                  height: displayHeight * 0.09,
+                  child: Column(
+                    children: [
+                      Text(
+                        "LOGIN",
+                        style: TextStyle(
+                            fontSize: 28,
+                            color: Color(0xffffb969),
+                            fontWeight: FontWeight.bold),
                       ),
-                    ),
-                    Row(
-                      children: [
-                        GestureDetector(
-                            onTap: () {
-                              MovetoRegister();
-                            },
-                            child: Text("회원가입")),
-                        GestureDetector(onTap: () {}, child: Text("아이디 찾기")),
-                        GestureDetector(onTap: () {}, child: Text("비밀번호 찾기")),
-                      ],
-                    ),
-                    Container(
-                      width: 40,
-                      height: 2,
-                      color: Colors.yellow,
-                    ),
-                  ],
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        width: displayWidth * 0.8,
+                        height: 5,
+                        color: Color(0xffffb969),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ),
-            Flexible(
-              flex: 2,
-              child: Container(
-                child: SizedBox(),
-              ),
-            ),
-            Flexible(
-              flex: 4,
-              child: Container(
-                child: Column(
-                  children: [
-                    Container(
-                      child: Text("카카오 로그인"),
-                    ),
-                    Container(
-                      child: Text("구글 로그인"),
-                    )
-                  ],
+                Container(
+                  height: displayHeight * 0.25,
+                  child: Column(
+                    children: [
+                      Form(
+                          child: Column(
+                        children: [
+                          Container(
+                            width: displayWidth * 0.8,
+                            decoration: BoxDecoration(
+                              color: Color(0xffffdfa166).withOpacity(0.4),
+                              borderRadius: BorderRadius.circular(3),
+                            ),
+                            child: TextFormField(
+                              controller: LoginIDtextController,
+                              textInputAction: TextInputAction.next,
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: '아이디를 입력하세요.',
+                                hintStyle: TextStyle(
+                                    color: Color(0xffffdfa166), fontSize: 18),
+                                prefixIcon: Icon(
+                                  Icons.mail_outline,
+                                  size: 30,
+                                  color: Color(0xffffdfa166),
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Container(
+                            width: displayWidth * 0.8,
+                            decoration: BoxDecoration(
+                              color: Color(0xffffdfa166).withOpacity(0.4),
+                              borderRadius: BorderRadius.circular(3),
+                            ),
+                            child: TextFormField(
+                              controller: LoginPWtextController,
+                              textInputAction: TextInputAction.done,
+                              keyboardType: TextInputType.text,
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: '비밀번호를 입력하세요.',
+                                hintStyle: TextStyle(
+                                    color: Color(0xffffdfa166), fontSize: 18),
+                                prefixIcon: Icon(
+                                  Icons.settings,
+                                  size: 30,
+                                  color: Color(0xffffdfa166),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      )),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Container(
+                        width: displayWidth * 0.8,
+                        child: Row(
+                          children: [
+                            Checkbox(
+                                value: _isChecked,
+                                onChanged: (value) {
+                                  setState(() {
+                                    _isChecked = !_isChecked;
+                                  });
+                                }),
+                            Text("로그인을 유지하겠습니까?")
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
+                Container(height: displayHeight * 0.03, child: SizedBox()),
+                Container(
+                  height: displayHeight * 0.15,
+                  child: Column(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          MovetoMain();
+                        },
+                        child: Container(
+                            width: displayWidth * 0.8,
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                              child: Center(
+                                  child: Text("로그인",
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold))),
+                            ),
+                            color: Color(0xffffb969)),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            GestureDetector(
+                                onTap: () {
+                                  MovetoRegister();
+                                },
+                                child: Padding(
+                                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                                  child: Container(
+                                      child: Text("회원가입",
+                                          style: TextStyle(
+                                            color: Color(0xffffb969),
+                                          ))),
+                                )),
+                            Container(
+                              height: 15,
+                              width: 2,
+                              color: Color(0xffffb969),
+                            ),
+                            GestureDetector(
+                                onTap: () {},
+                                child: Padding(
+                                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                                  child: Container(
+                                      child: Text(
+                                    "아이디 찾기",
+                                    style: TextStyle(
+                                      color: Color(0xffffb969),
+                                    ),
+                                  )),
+                                )),
+                            Container(
+                              height: 15,
+                              width: 2,
+                              color: Color(0xffffb969),
+                            ),
+                            GestureDetector(
+                                onTap: () {},
+                                child: Padding(
+                                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                                  child: Container(
+                                      child: Text(
+                                    "비밀번호 찾기",
+                                    style: TextStyle(
+                                      color: Color(0xffffb969),
+                                    ),
+                                  )),
+                                )),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 25,
+                      ),
+                      Container(
+                        width: displayWidth * 0.8,
+                        height: 5,
+                        color: Color(0xffffb969),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  height: displayHeight * 0.09,
+                  child: SizedBox(),
+                ),
+                Container(
+                  height: displayHeight * 0.16,
+                  child: Column(
+                    children: [
+                      Container(
+                        child: Text("카카오 로그인"),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        child: Text("구글 로그인"),
+                      )
+                    ],
+                  ),
+                ),
+              ],
             ),
-            Flexible(
-              flex: 3,
-              child: Container(
-                child: SizedBox(),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
