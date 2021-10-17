03b74a1594029d6614fd7f961aa0d1beeacba14f
MintKangaroo <yjun273@gmail.com>
2021-10-17 22:05:09 +0900

    서버와 통신 성공2

--- FooRip_APP/foorip_app_project/lib/Function/ServerContact.dart
+++ FooRip_APP/foorip_app_project/lib/Function/ServerContact.dart
@@ -1,12 +1,16 @@
+
+
 import 'package:http/http.dart' as http;
 import 'dart:async';
+import 'dart:convert';
 
 class ServerContact{
 
   Future<String> LoginContact() async{
-    var url = Uri.parse('http://127.0.0.1:5000/UserData');
-    var response =  await http.post(url, body: {"user": "python"});
-    print(response);
-    return "response";
+    var url = Uri.parse('https://fooripbackend.herokuapp.com/');
+    //var response =  await http.post(url, body: {"user": "python"});
+    var response = await http.get(url);
+    var responsedecode = utf8.decode(response.bodyBytes);
+    return responsedecode;
   }
 }
