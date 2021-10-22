caa016946c772df674c1cce1ffcc6f3154c03d4b
MintKangaroo <yjun273@gmail.com>
2021-10-22 23:41:50 +0900

    마이페이지 구현

--- FooRip_APP/foorip_app_project/lib/Pages/MyPage.dart
+++ FooRip_APP/foorip_app_project/lib/Pages/MyPage.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-
 class MyPage extends StatefulWidget {
-  const MyPage({ Key? key }) : super(key: key);
+  const MyPage({Key? key}) : super(key: key);
 
   @override
   _MyPageState createState() => _MyPageState();
 }
 
 class _MyPageState extends State<MyPage> {
-
   //bottomNavigator 변수 정의
   int _selectedIndex = 1;
   final List _children = [
@@ -25,11 +23,9 @@ class _MyPageState extends State<MyPage> {
       setState(() {
         _selectedIndex = index;
       });
-    }
-    else if (index == 0) {
+    } else if (index == 0) {
       Get.offAllNamed('/MainPage');
-    } 
-    else {
+    } else {
       setState(() {
         _selectedIndex = index;
         Get.toNamed(_children[index]);
@@ -37,74 +33,129 @@ class _MyPageState extends State<MyPage> {
     }
   }
 
+  void MovetoSetting(){
+    Get.toNamed('/SettingPage');
+  }
+
   @override
   Widget build(BuildContext context) {
     //Display width, height 구하기
     var displayWidth = MediaQuery.of(context).size.width;
     var displayHeight = MediaQuery.of(context).size.height;
-    
-    return Scaffold(
-      body: Column(children: [
-        Flexible(flex: 1,child: Container(color: Colors.orange,),),
-        Flexible(flex: 1,child: Container(color: Colors.white,),)
-      ],),
-      bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          backgroundColor: Colors.white,
-          currentIndex: _selectedIndex,
-          onTap: (_onItemTapped),
-          showUnselectedLabels: false,
-          selectedItemColor: Colors.black,
-          items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              activeIcon: Icon(
-                Icons.home,
-                color: Color(0xffffb969),
+
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.orange,
+          body: Column(
+            children: [
+              Flexible(
+                flex: 1,
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SizedBox(width: 25,),
+                        Image.asset("assets/images/fooriptextlogo.png",width: displayWidth*0.4,),
+                        GestureDetector(
+                          onTap: (){
+                            MovetoSetting();
+                          },
+                          child: Icon(Icons.settings, size: 35,))
+                      ],
+                    ),
+
+                  ],
+                )
               ),
-              icon: Icon(
-                Icons.home,
-                color: Color(0xffffb969).withOpacity(0.5),
-                size: 30,
+              Flexible(
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(40.0),
+                          topRight: Radius.circular(40.0)),
+                    ),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            SizedBox(),
+                            Text("ACHIEVEMENTS"),
+                            Spacer(),
+                            Container(),
+                            SizedBox(),
+                          ],
+                        )
+                      ],
+                    ),
+                    
+                  ),
+                ),
+              )
+            ],
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: Colors.white,
+            currentIndex: _selectedIndex,
+            onTap: (_onItemTapped),
+            showUnselectedLabels: false,
+            selectedItemColor: Colors.black,
+            items: <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                activeIcon: Icon(
+                  Icons.home,
+                  color: Color(0xffffb969),
+                ),
+                icon: Icon(
+                  Icons.home,
+                  color: Color(0xffffb969).withOpacity(0.5),
+                  size: 30,
+                ),
+                label: "Home",
               ),
-              label: "Home",
-            ),
-            BottomNavigationBarItem(
-              activeIcon: Icon(
-                Icons.account_box,
-                color: Color(0xffffb969),
+              BottomNavigationBarItem(
+                activeIcon: Icon(
+                  Icons.account_box,
+                  color: Color(0xffffb969),
+                ),
+                icon: Icon(
+                  Icons.account_box,
+                  color: Color(0xffffb969).withOpacity(0.5),
+                  size: 30,
+                ),
+                label: "MyPage",
               ),
-              icon: Icon(
-                Icons.account_box,
-                color: Color(0xffffb969).withOpacity(0.5),
-                size: 30,
+              BottomNavigationBarItem(
+                activeIcon: Icon(
+                  Icons.favorite,
+                  color: Color(0xffffb969),
+                ),
+                icon: Icon(
+                  Icons.favorite,
+                  color: Color(0xffffb969).withOpacity(0.5),
+                  size: 30,
+                ),
+                label: "Favorite",
               ),
-              label: "MyPage",
-            ),
-            BottomNavigationBarItem(
-              activeIcon: Icon(
-                Icons.favorite,
-                color: Color(0xffffb969),
+              BottomNavigationBarItem(
+                activeIcon: Icon(
+                  Icons.military_tech,
+                  color: Color(0xffffb969),
+                ),
+                icon: Icon(
+                  Icons.military_tech,
+                  color: Color(0xffffb969).withOpacity(0.5),
+                  size: 30,
+                ),
+                label: "Medal",
               ),
-              icon: Icon(
-                Icons.favorite,
-                color: Color(0xffffb969).withOpacity(0.5),
-                size: 30,
-              ),
-              label: "Favorite",
-            ),
-            BottomNavigationBarItem(
-              activeIcon: Icon(
-                Icons.military_tech,
-                color: Color(0xffffb969),
-              ),
-              icon: Icon(
-                Icons.military_tech,
-                color: Color(0xffffb969).withOpacity(0.5),
-                size: 30,
-              ),
-              label: "Medal",
-            ),
-          ],)
+            ],
+          )),
     );
   }
 }
