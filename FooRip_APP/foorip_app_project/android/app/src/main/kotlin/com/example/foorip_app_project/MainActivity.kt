773fb041dc9215bf6d0edeef9c89ffc69011ac2e
MintKangaroo <yjun273@gmail.com>
2021-10-31 01:06:18 +0900

    Revert "코틀린 광고 정의"
    This reverts commit 2b3e2bcf41400d59011709eb06df8ae264e4ffbc.

--- FooRip_APP/foorip_app_project/android/app/src/main/kotlin/com/example/foorip_app_project/MainActivity.kt
+++ FooRip_APP/foorip_app_project/android/app/src/main/kotlin/com/example/foorip_app_project/MainActivity.kt
@@ -4,35 +4,3 @@ import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
 }
-
-import android.graphics.Color
-import android.view.LayoutInflater
-import android.widget.TextView
-import com.google.android.gms.ads.nativead.NativeAd
-import com.google.android.gms.ads.nativead.NativeAdView
-import io.flutter.plugins.googlemobileads.GoogleMobileAdsPlugin.NativeAdFactory
-import java.util.Map
-
-
-
-internal class NativeAdFactoryExample(layoutInflater: LayoutInflater) : NativeAdFactory {
-    private val layoutInflater: LayoutInflater
-    @Override
-    fun createNativeAd(
-            nativeAd: NativeAd, customOptions: Map<String?, Object?>?): NativeAdView {
-        val adView: NativeAdView = layoutInflater.inflate(R.layout.my_native_ad, null) as NativeAdView
-        val headlineView: TextView = adView.findViewById(R.id.ad_headline)
-        val bodyView: TextView = adView.findViewById(R.id.ad_body)
-        headlineView.setText(nativeAd.getHeadline())
-        bodyView.setText(nativeAd.getBody())
-        adView.setBackgroundColor(Color.YELLOW)
-        adView.setNativeAd(nativeAd)
-        adView.setBodyView(bodyView)
-        adView.setHeadlineView(headlineView)
-        return adView
-    }
-
-    init {
-        this.layoutInflater = layoutInflater
-    }
-}
