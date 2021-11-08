f70538ae2ca47e12c7ff3c6878d13828b7f1fc8a
sun922 <blue470nm0922@gmail.com>
2021-11-08 10:53:01 +0900

    Settingpage 제작(수정 1필요)

--- FooRip_APP/foorip_app_project/lib/Pages/SettingPage.dart
+++ FooRip_APP/foorip_app_project/lib/Pages/SettingPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:developer';
 import 'package:foorip_app_project/Function/DataSaveCheck.dart';
+import 'package:foorip_app_project/Pages/MyPage.dart';
 import 'package:get/get.dart';
 import 'LoginPage.dart';
 import 'package:flutter/services.dart';
@@ -24,8 +25,7 @@ class _SettingPageState extends State<SettingPage> {
     SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.top]);
     //statusBar 색상 변경
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-      statusBarColor: Colors.grey 
-      , // 원하는 색
+      statusBarColor: Colors.grey[300], // 원하는 색
     ));
     //ststusBar 글자 색 변경하고 싶다
 
@@ -37,20 +37,121 @@ class _SettingPageState extends State<SettingPage> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Container(
-                height: displayHeight*0.1,
+                padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                height: displayHeight * 0.065,
                 width: displayWidth * 1,
                 child: Image.asset("assets/images/fooriptextlogo.png"),
-                
               ),
               Container(
-                padding: const EdgeInsets.fromLTRB(0, 0, 300, 0),//왼 위 오 아래
-                height: displayHeight * 0.05,
-                width: displayWidth *1,
-                child: Icon(
-                  Icons.arrow_back,
-                  size: 35,
+                padding: const EdgeInsets.fromLTRB(0, 0, 300, 500), //왼 위 오 아래
+                height: displayHeight * 0.09,
+                width: displayWidth * 1,
+                child: InkWell(//이거 왜 안될까요?
+                  onTap: () {
+                    Get.toNamed('/MainPage');
+                  },
+                  child: Icon(
+                    Icons.arrow_back_rounded,
+                    size: 35,
+                  ),
                 ),
-              )
+              ),
+              Container(
+                  //color: Colors.black,
+                  width: displayWidth * 0.8,
+                  child: Column(
+                    children: [
+                      //문의사항
+                      Container(
+                        //color: Colors.grey[100],
+                        height: displayHeight * 0.08,
+                        child: Row(
+                          children: [
+                            Padding(padding: EdgeInsets.fromLTRB(6, 0, 0, 0)),
+                            Icon(
+                              Icons.help_outline,
+                              size: 25,
+                            ),
+                            Padding(padding: EdgeInsets.fromLTRB(10, 0, 0, 0)),
+                            Text(
+                              "문의사항",
+                              style: TextStyle(fontSize: 18),
+                            ),
+                            Padding(
+                                padding: EdgeInsets.fromLTRB(
+                                    131, 0, 0, 0)), //왼 위 오 아래
+                            Icon(
+                              Icons.chevron_right_rounded,
+                              size: 40,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 1,
+                        color: Color(0xffffC4C4C4),
+                      ),
+                      //로그아웃
+                      Container(
+                        //color: Colors.grey[100],
+                        height: displayHeight * 0.08,
+                        child: Row(
+                          children: [
+                            Padding(padding: EdgeInsets.fromLTRB(6, 0, 0, 0)),
+                            Icon(
+                              Icons.logout,
+                              size: 25,
+                            ),
+                            Padding(padding: EdgeInsets.fromLTRB(10, 0, 0, 0)),
+                            Text(
+                              "로그아웃",
+                              style: TextStyle(fontSize: 18),
+                            ),
+                            Padding(
+                                padding: EdgeInsets.fromLTRB(
+                                    131, 0, 0, 0)), //왼 위 오 아래
+                            InkWell(
+                              onTap: () {
+                                DataSaveCheck.LogOutData();
+                                Get.offAll(LoginPage());
+                              },
+                              child: Icon(
+                                Icons.chevron_right_rounded,
+                                size: 40,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 1,
+                        color: Color(0xffffC4C4C4),
+                      ),
+                      //버전
+                      Container(
+                        //color: Colors.grey[100],
+                        height: displayHeight * 0.08,
+                        child: Row(
+                          children: [
+                            Padding(padding: EdgeInsets.fromLTRB(6, 0, 0, 0)),
+                            Icon(
+                              Icons.language_rounded,
+                              size: 25,
+                            ),
+                            Padding(padding: EdgeInsets.fromLTRB(10, 0, 0, 0)),
+                            Text(
+                              "v. 1. 1. 0",
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 1,
+                        color: Color(0xffffC4C4C4),
+                      ),
+                    ],
+                  ))
             ],
           ),
         ),
